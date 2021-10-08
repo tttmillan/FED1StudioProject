9bc54e0e73d917bcd5e9f4f530ffdcd22178aba4
Anthony Millan <tttmillan@yahoo.com>
2021-10-08 08:10:00 -0500

    update 10/8

---
+++ FED1StudioProject/js/nav.js
@@ -0,0 +1,16 @@
+﻿function openAdminNav() {
+    document.getElementById('admin-btn').style.display = 'none';
+    document.getElementById('admin-nav').style.width = '350px';
+}
+
+function closeAdminNav() {
+    document.getElementById('admin-btn').style.display = 'block';
+    document.getElementById('admin-nav').style.width = '0';
+}
+
+
+
+
+
+
+
